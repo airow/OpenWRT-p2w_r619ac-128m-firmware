e16f92f444409327972325137229b33828faf4dc
zhangch <airow@qq.com>
2020-02-21 01:33:55 +0800

    Modify pptpd.include

--- diy.sh
+++ diy.sh
@@ -7,3 +7,8 @@
 #=================================================
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+
+# Remove forwarding_rule
+# iptables -A forwarding_rule -i ppp+ -j ACCEPT
+# iptables -A forwarding_rule -o ppp+ -j ACCEPT
+sed -i 's/iptables -A/#iptables -A/g' package/lean/luci-app-pptp-server/root/etc/pptpd.include
