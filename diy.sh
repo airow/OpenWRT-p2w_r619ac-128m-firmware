c1443b91347d8b497e38490ad4640a4fb762d25b
zhangch <airow@qq.com>
2020-05-02 01:00:10 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -11,4 +11,4 @@ sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generat
 # Remove forwarding_rule
 # iptables -A forwarding_rule -i ppp+ -j ACCEPT
 # iptables -A forwarding_rule -o ppp+ -j ACCEPT
-sed -i 's/iptables -A/#iptables -A/g' package/lean/luci-app-pptp-server/root/etc/pptpd.include
+#sed -i 's/iptables -A/#iptables -A/g' package/lean/luci-app-pptp-server/root/etc/pptpd.include
