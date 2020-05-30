2aed324fcd765988b40bb2a85701734df1e8f720
zhangch <airow@qq.com>
2020-05-30 21:25:25 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -9,4 +9,9 @@
 #============================================================
 
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+
+# Remove forwarding_rule
+# iptables -A forwarding_rule -i ppp+ -j ACCEPT
+# iptables -A forwarding_rule -o ppp+ -j ACCEPT
+#sed -i 's/iptables -A/#iptables -A/g' package/lean/luci-app-pptp-server/root/etc/pptpd.include
